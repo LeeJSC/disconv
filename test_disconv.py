1f299291e64da675afdef72c34b44550a0ebdb41
Lee Johns Shaji <100472821+LeeJSC@users.noreply.github.com>
2022-08-22 10:09:30 +0530

    Add files via upload

--- test_disconv.py
+++ test_disconv.py
@@ -5,5 +5,5 @@ def test_tonum():
 
 def test_tofloat():
     assert foo.tofloat([213,21541,65,47,[['g']],{1:'s'}])==[213.0, 21541.0, 65.0, 47.0, [[[103.0]]], {1: [115.0]}]
-def test_tonum():
+def test_tostr():
     assert foo.tostr([213,21541,65,47,[['g']],{1:'s'}])==['213','21541','65','47',[['g']],{1:'s'}]
