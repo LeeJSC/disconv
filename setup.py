3779b9042aa21d2b3c3a0ace93b9d946a1a5ba08
leejs <leejohnsdecarmel@gmail.com>
2022-08-23 20:11:32 +0530

    license update

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(name='disconv',
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
-            "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+            "License :: MIT",
             "Operating System :: OS Independent",
 
       ],
